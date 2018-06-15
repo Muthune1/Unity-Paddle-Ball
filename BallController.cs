3e53bbbd091517211b0f1f82ac567fcb7825075b
Muthune1 <40231559+Muthune1@users.noreply.github.com>
2018-06-15 16:03:51 +0530

    Add files via upload

---
+++ BallController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	private Rigidbody rb;
+	public float velocity;
+	Vector2 direction;
+	public float MaxX;
+	public float MaxY;
+	float radius;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+		direction = Vector2.one.normalized; //direction is (1,1) normalized
+		radius = transform.localScale.x/2; //radius gets the half the width of the object
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (direction * Time.deltaTime);
+		if (transform.position.x-radius < -MaxX || transform.position.x+radius > MaxX) 
+		{
+			
+			//direction.x = -direction.x;
+			GetComponent<Rigidbody> ().velocity = -GetComponent<Rigidbody> ().velocity;
+
+		}
+
+		if (transform.position.y+radius > MaxY ) 
+		{
+			Debug.Log ("A");
+			//direction.x = -direction.x;
+			GetComponent<Rigidbody> ().velocity = -GetComponent<Rigidbody> ().velocity;
+
+		}
+	}
+
+
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Paddle") {
+			float vel = other.gameObject.GetComponent<PaddleControl> ().velocity;
+			rb.AddForce (new Vector3 (-vel * 20, velocity, 0));
+		}
+	}
+}
---
+++ PaddleControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour {
+	public float speed; //rate of paddle movement
+	public float maxY;  //defines boundaries
+	public float maxX;
+	public float velocity;
+	private Rigidbody rb;
+	float lastXPos;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal"); //Get keyboard input
+
+//		Vector2 movement = new Vector2 (moveHorizontal,0.0f);//Store movement type in a variable
+		
+//		rb.AddForce (movement * speed);	// Combine movement and speed to force movement on rigid body
+
+		float currentXPos = lastXPos;
+		Vector3 pos = new Vector3(transform.position.x+(Time.deltaTime*moveHorizontal*speed),transform.position.y,transform.position.z); 
+		transform.position = pos;     //Hold position when it reaches boundary
+		velocity = (currentXPos - pos.x) / Time.deltaTime;
+		lastXPos=pos.x;
+
+
+		if (transform.position.x < -maxX || transform.position.x > maxX) {
+
+			pos =new Vector3(transform.position.x-(Time.deltaTime*moveHorizontal*speed),transform.position.y,transform.position.z);
+			transform.position = pos; 
+			pos.x = Mathf.Clamp (pos.x, maxX, maxX); //Define boundaries
+
+		}
+
+
+
+}
+
+}
